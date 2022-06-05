de924f0bef7c87faafd501c4c914c8b35e41d6be
khodanit <khodanit@leadingtraining.co.za>
2022-06-05 22:53:17 +0200

    Exercise 13

---
+++ 5. Tuples/tuples.py
@@ -0,0 +1,9 @@
+"""
+Given the tuple: fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+13. Print the third, fourth, and fifth item.
+"""
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+
+print(fruits[2], fruits[3], fruits[4], sep=" ")
