1f8517f1eeaf4570acd5b21f35c691c3435b4374
khodanit <khodanit@leadingtraining.co.za>
2022-06-05 23:03:20 +0200

    Exercise 15

--- 6. Dictionaries/dictonaries.py
+++ 6. Dictionaries/dictonaries.py
@@ -6,12 +6,12 @@ car =	{
   "year": 1964
 }
 
-14. Add the 'colour' attribute to the car dictionary and declare it as 'red'
+15. Change the 'year' attribute to '2020'
 """
 car = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
 print(car)
 
-car.update({"colour": "red"})
+car["year"] = 2020
 
 print(car)
